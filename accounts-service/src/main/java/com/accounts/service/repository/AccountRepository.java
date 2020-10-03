1ed67108b15c4f7b29205ddd7d8c59760de03491
PoojaGowda114 <62004090+PoojaGowda114@users.noreply.github.com>
2020-10-03 12:01:48 +0530

    Update AccountRepository.java

--- accounts-service/src/main/java/com/accounts/service/repository/AccountRepository.java
+++ accounts-service/src/main/java/com/accounts/service/repository/AccountRepository.java
@@ -1,9 +1,10 @@
 package com.accounts.service.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.accounts.service.model.Account;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
 
 }
