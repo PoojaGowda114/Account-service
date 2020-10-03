7ccec336d31a6a83170c178e64988669a9ba1b68
PoojaGowda114 <62004090+PoojaGowda114@users.noreply.github.com>
2020-10-03 12:03:35 +0530

    First commit

--- accounts-service/src/main/java/com/accounts/service/repository/AccountRepository.java
+++ accounts-service/src/main/java/com/accounts/service/repository/AccountRepository.java
@@ -2,6 +2,7 @@ package com.accounts.service.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.accounts.service.model.Account;
 
 @Repository
